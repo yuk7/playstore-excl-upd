a3a0bbde35944ad25e96fb19a2f2e8d1e6c95315
yuk7 <yukx00@gmail.com>
2021-03-06 23:32:33 +0900

    service.sh: disable auto update with wifi

--- service.sh
+++ service.sh
@@ -44,8 +44,9 @@ END
     am force-stop ${PS_PKG_NAME}
     fi
 
-    if grep -q "<boolean name=\"auto_update_enabled\" value=\"true\" />" "$PS_DATA_DIR/shared_prefs/finsky.xml"; then
+    if grep -q -e "<boolean name=\"auto_update_enabled\" value=\"true\" />" -e "<boolean name=\"update_over_wifi_only\" value=\"true\" />" "$PS_DATA_DIR/shared_prefs/finsky.xml"; then
         sed -i.bak -e 's#<boolean name="auto_update_enabled" value="true" />#<boolean name="auto_update_enabled" value="false" />#g' "$PS_DATA_DIR/shared_prefs/finsky.xml"
+        sed -i.bak -e 's#<boolean name="update_over_wifi_only" value="true" />#<boolean name="update_over_wifi_only" value="false" />#g' "$PS_DATA_DIR/shared_prefs/finsky.xml"
         am force-stop ${PS_PKG_NAME}
     fi
 
